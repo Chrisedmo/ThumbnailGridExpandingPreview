2f969853413b831b963d1c141f927fb75d747f9b
Chris Mousdale <chris@mousdale.com>
2013-04-04 08:15:08 +0100

    Adding in IE7/8 support
    Need to test:
    
    "A few people have mentioned IE7 / IE8 support… replacing the
    jquery.debouncedresize.js function at the start of grid.js with
    jquery.throttledresize.js found at the GitHub page here seems to fix
    the expander issue."

--- js/grid.js
+++ js/grid.js
@@ -38,6 +38,65 @@ $special = $event.special.debouncedresize = {
 	},
 	threshold: 250
 };
+// IE 7/8 SUPPORT? : A few people have mentioned IE7 / IE8 support… replacing the jquery.debouncedresize.js function at the start of grid.js with jquery.throttledresize.js found at the GitHub page here seems to fix the expander issue.
+/*
+ * throttledresize: special jQuery event that happens at a reduced rate compared to "resize"
+ *
+ * latest version and complete README available on Github:
+ * https://github.com/louisremi/jquery-smartresize
+ *
+ * Copyright 2012 @louis_remi
+ * Licensed under the MIT license.
+ *
+ * This saved you an hour of work? 
+ * Send me music http://www.amazon.co.uk/wishlist/HNTU0468LQON
+ */
+// (function($) {
+
+// var $event = $.event,
+// 	$special,
+// 	dummy = {_:0},
+// 	frame = 0,
+// 	wasResized, animRunning;
+
+// $special = $event.special.throttledresize = {
+// 	setup: function() {
+// 		$( this ).on( "resize", $special.handler );
+// 	},
+// 	teardown: function() {
+// 		$( this ).off( "resize", $special.handler );
+// 	},
+// 	handler: function( event, execAsap ) {
+// 		// Save the context
+// 		var context = this,
+// 			args = arguments;
+
+// 		wasResized = true;
+
+// 		if ( !animRunning ) {
+// 			setInterval(function(){
+// 				frame++;
+
+// 				if ( frame > $special.threshold && wasResized || execAsap ) {
+// 					// set correct event type
+// 					event.type = "throttledresize";
+// 					$event.dispatch.apply( context, args );
+// 					wasResized = false;
+// 					frame = 0;
+// 				}
+// 				if ( frame > 9 ) {
+// 					$(dummy).stop();
+// 					animRunning = false;
+// 					frame = 0;
+// 				}
+// 			}, 30);
+// 			animRunning = true;
+// 		}
+// 	},
+// 	threshold: 0
+// };
+
+// })(jQuery);
 
 // ======================= imagesLoaded Plugin ===============================
 // https://github.com/desandro/imagesloaded
